b87fa9b4ae6e5c5685dcfeb468b506631584f30a
moondustmc <mc.moondust@gmail.com>
2026-05-14 19:40:16 -0400

    Bypass permission

--- src/main/java/com/moondust/antiflowerpottheft/Antiflowerpottheft.java
+++ src/main/java/com/moondust/antiflowerpottheft/Antiflowerpottheft.java
@@ -1,16 +1,36 @@
 package com.moondust.antiflowerpottheft;
 
+import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Antiflowerpottheft extends JavaPlugin {
 
+    private RegionManager regionManager;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        saveDefaultConfig();
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+
+        regionManager = new RegionManager(this);
+        regionManager.load();
+
+        AntiFlowerPotTheftCommand commandHandler = new AntiFlowerPotTheftCommand(this, regionManager);
+        PluginCommand command = getCommand("flowerpot");
+        if (command != null) {
+            command.setExecutor(commandHandler);
+            command.setTabCompleter(commandHandler);
+        }
+
+        getServer().getPluginManager().registerEvents(new PotProtectionListener(this, regionManager), this);
+        getLogger().info("Loaded " + regionManager.getRegions().size() + " protected flower pot region(s).");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        if (regionManager != null) {
+            regionManager.save();
+        }
     }
 }
